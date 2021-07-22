54bf331635ad7a62562c785b6324dbad49b40942
Lucas Corte <corte1994.lc@gmail.com>
2021-07-22 15:16:20 -0300

    imlementação da função de checkWinnerFor

--- static/js/script.js
+++ static/js/script.js
@@ -78,3 +78,26 @@ const checkGame = () => {
     playing = false;
   }
 }
+
+const checkWinnerFor(player) {
+  let possibilities = [
+    'a1,a2,a3',
+    'b1,b2,b3',
+    'c1,c2,c3',
+
+    'a1,b1,c1',
+    'a2,b2,c2',
+    'a3,b3,c3',
+
+    'a1,b2,c3',
+    'a3,b2,c1'
+  ]
+  
+  for(let i in possibilities) {
+    let pArray = possibilities[i].split(','); //cria array [a1, a2, a3]
+    let hasWon = pArray.every(option => square[option] === player);
+    if(hasWon) return true;
+  }
+  
+  return false;
+}
